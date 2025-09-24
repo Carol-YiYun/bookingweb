310e552b60271a44330b514c7b93a3195fadef43
Carol Lin <167300790+Carol-YiYun@users.noreply.github.com>
2025-09-25 00:12:36 +0800

    modify db.js

--- server/db.js
+++ server/db.js
@@ -1,27 +1,27 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-let ready = false;
-
-export async function connectDB() {
-  if (ready || mongoose.connection.readyState === 1) return;
-  await mongoose.connect(process.env.MONGODB);
-  ready = true;
-
-  mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected!");
-  });
-  mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB disconnected!");
-  });
-}
-
-// let mongooseInstance = null;
+// let ready = false;
 
 // export async function connectDB() {
-//   if (mongooseInstance) return mongooseInstance;
-//   const mongoose = (await import("mongoose")).default;
-//   const conn = await mongoose.connect(process.env.MONGODB);
-//   mongooseInstance = conn;
-//   return conn;
+//   if (ready || mongoose.connection.readyState === 1) return;
+//   await mongoose.connect(process.env.MONGODB);
+//   ready = true;
+
+//   mongoose.connection.on("connected", () => {
+//     console.log("MongoDB connected!");
+//   });
+//   mongoose.connection.on("disconnected", () => {
+//     console.log("MongoDB disconnected!");
+//   });
 // }
+
+let mongooseInstance = null;
+
+export async function connectDB() {
+  if (mongooseInstance) return mongooseInstance;
+  const mongoose = (await import("mongoose")).default;
+  const conn = await mongoose.connect(process.env.MONGODB);
+  mongooseInstance = conn;
+  return conn;
+}
 
