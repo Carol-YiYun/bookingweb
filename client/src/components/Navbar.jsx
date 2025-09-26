fe4230f854a40195e85f8e3190d3264113a4de34
Carol Lin <167300790+Carol-YiYun@users.noreply.github.com>
2025-09-26 20:01:13 +0800

    fix useState not defined

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import "./navbar.scss"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
