779d925a135a086254db7889bb1209f1ba92e829
Carol Lin <167300790+Carol-YiYun@users.noreply.github.com>
2025-09-26 19:57:30 +0800

    fix tooltip

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,6 +13,10 @@ const Navbar = ({ type }) => {
         dispatch({ type: logout })
     }
 
+    // 紀錄哪個 item 要顯示 tooltip
+    const [activeTip, setActiveTip] = useState(null);
+
+
     return (
         <div className={`navbar ${type}`}>
             <div className="navbarContainer ">
@@ -50,21 +54,49 @@ const Navbar = ({ type }) => {
                             <FontAwesomeIcon icon={faBed} />
                             <span >住宿</span>
                         </div>
-                        <div className="item" onMouseEnter={() => alert("尚未開發")}>
+                        <div 
+                            className="item" 
+                            onMouseEnter={() => setActiveTip("plane")}
+                            onMouseLeave={() => setActiveTip(null)}
+                        >
                             <FontAwesomeIcon icon={faPlane} />
                             <span >航班</span>
+                            {activeTip === "plane" && (
+                                <div className="nav-tooltip">尚未開發</div>
+                            )}
                         </div>
-                        <div className="item" onMouseEnter={() => alert("尚未開發")}>
+                        <div 
+                            className="item" 
+                            onMouseEnter={() => setActiveTip("car")}
+                            onMouseLeave={() => setActiveTip(null)}
+                        >
                             <FontAwesomeIcon icon={faCar} />
                             <span >租車</span>
+                            {activeTip === "car" && (
+                                <div className="nav-tooltip">尚未開發</div>
+                            )}
                         </div>
-                        <div className="item" onMouseEnter={() => alert("尚未開發")}>
+                        <div 
+                            className="item" 
+                            onMouseEnter={() => setActiveTip("tour")}
+                            onMouseLeave={() => setActiveTip(null)}
+                        >
                             <FontAwesomeIcon icon={faToriiGate} />
                             <span >景點/活動</span>
+                            {activeTip === "tour" && (
+                                <div className="nav-tooltip">尚未開發</div>
+                            )}
                         </div>
-                        <div className="item" onMouseEnter={() => alert("尚未開發")}>
+                        <div 
+                            className="item" 
+                            onMouseEnter={() => setActiveTip("taxi")}
+                            onMouseLeave={() => setActiveTip(null)}
+                        >
                             <FontAwesomeIcon icon={faTaxi} />
                             <span >機場計程車</span>
+                            {activeTip === "taxi" && (
+                                <div className="nav-tooltip">尚未開發</div>
+                            )}
                         </div>
                     </div>
                 }
